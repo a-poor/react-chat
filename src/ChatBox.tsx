7106838364c595848b413057ca7b33ad75191e61
a-poor <45295232+a-poor@users.noreply.github.com>
2023-03-08 21:54:27 -0800

    Create starter components

---
+++ src/ChatBox.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import Box from '@mui/material/Box';
+
+export interface IChatBoxProps {}
+
+export function ChatBox({}: IChatBoxProps) {
+  return (
+    <></>
+  );
+}
---
+++ src/ChatMessage.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Box from '@mui/material/Box';
+
+export interface IChatMessageProps {
+  message: string;
+}
+
+export function ChatMessage({}: IChatMessageProps) {
+  return (
+    <></>
+  );
+}
